4f24136f184e7edbc9e379f5f0709efa5eb7e92b
fccagou <fccagou@gmail.com>
2015-07-26 18:08:41 +0200

    add: db9 class and test code as pyserialnotify

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # pynotif
-notification using python
+
+Notification using python
+
+## History
+
+I would like to get physical notifications to be informed when a new event
+occurs on my computer.
+
+I would like a solution with less electronic as possible.
+
+
+Many solutions exist :
+
+- using an [arduino](https://www.arduino.cc/en/Main/arduinoBoardNano)
+- using commercial solutions like [blink(1)](http://blink1.thingm.com/)
+- or DIY solution
+
+I've chosen DIY solution and begun begun with hardware I have in my cupboard, 
+a USB/Serial cable.
+
+I create the first python class [Db9](#Serial Db9)
+
+## Serial Db9
+
+Inspired by a linuxfocus article on [sled](http://linuxfocus.org/English/January2001/article186.shtml)
+the Db9 class use ioctl to switch on/off the output pins.
+
+The file [pyserialnotif](pyserialnotif) is an example.
+
+The tests are made on this hardware
+
+![Db9 test hardware](images/pyserialnotify_hw_test.png)
+
+
+
+
---
+++ db9/__init__.py
@@ -0,0 +1,54 @@
+
+import struct, termios, fcntl
+
+
+
+class Db9( object ) :
+
+
+
+
+    PIN1 = termios.TIOCM_CD
+    PIN2 = termios.TIOCM_SR
+    PIN3 = termios.TIOCM_ST
+    PIN4 = termios.TIOCM_DTR
+    PIN5 = -1
+    PIN6 = termios.TIOCM_DSR
+    PIN7 = termios.TIOCM_RTS
+    PIN8 = termios.TIOCM_CTS
+    PIN9 = termios.TIOCM_RI
+
+    ALL_OUTPUT = PIN3 | PIN4 | PIN7
+
+    __tty = None
+    __state = ' ' * 8
+    __fd = None
+    __pins = ( ALL_OUTPUT, PIN1, PIN2, PIN3, PIN4, PIN5, PIN6, PIN7, PIN8, PIN9)
+
+
+    def __init__(self, tty):
+        self.__tty = tty
+
+
+    def connect(self):
+        print "[+] - using  %s" % self.__tty
+        self.__fd = open(self.__tty, 'w+')
+        self.__state = fcntl.ioctl(self.__fd, termios.TIOCMGET, '\000\000\000\000\000\000\000\000')
+
+    def switch_on( self, pin):
+        print "[+] - switch on pin %s" % self.__pins.index(pin)
+        self.__state = fcntl.ioctl(self.__fd, termios.TIOCMSET, struct.pack('l', struct.unpack('l', self.__state)[0] | pin))
+
+
+    def switch_off( self, pin):
+        print "[+] - switch off pin %s" % self.__pins.index(pin)
+        self.__state = fcntl.ioctl(self.__fd, termios.TIOCMSET, struct.pack('l', struct.unpack('l', self.__state)[0] & ~pin))
+
+
+    def disconnect( self ):
+        self.__fd.close()
+
+    def write( self , datas ):
+        self.__fd.write(datas)
+
+
