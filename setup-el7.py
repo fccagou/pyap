291cd9b4975c176a7e1a7673eccf36fc5df89ce6
fccagou <fccagou@gmail.com>
2017-01-16 21:04:57 +0100

    fix(data): udev rules / version 0.3.1

--- setup-el7.py
+++ setup-el7.py
@@ -19,7 +19,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.2',
+        version='0.3.1',
         description='Python notification system',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
