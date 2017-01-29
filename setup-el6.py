a6aba5839ad7efa09ed8c9e3e0e06d4c16e656e2
fccagou <fccagou@gmail.com>
2017-01-29 17:51:43 +0100

    refactor(conf): update conf arguments - v0.4.0 - breaks previous version

--- setup-el6.py
+++ setup-el6.py
@@ -18,7 +18,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.3.2',
+        version='0.4.0',
         description='PYthon Alert Processor',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
