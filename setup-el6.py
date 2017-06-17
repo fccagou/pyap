f1f558ba6b071383814a357ad258c05981c9d901
fccagou <fccagou@gmail.com>
2017-06-17 15:53:00 +0200

    fix(pyap): add a fallback when a remote poller is unaccessible

--- setup-el6.py
+++ setup-el6.py
@@ -18,7 +18,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.4.1',
+        version='0.4.2',
         description='PYthon Alert Processor',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
