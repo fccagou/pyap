fde2dca3e4079ea8eb7476611e3490531645027b
fccagou <fccagou@gmail.com>
2017-06-26 22:13:10 +0200

    v0.4.5: fix errors introduced in v0.4.3

--- setup-el6.py
+++ setup-el6.py
@@ -18,7 +18,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.4.4',
+        version='0.4.5',
         description='PYthon Alert Processor',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
--- setup-el7.py
+++ setup-el7.py
@@ -19,7 +19,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.4.4',
+        version='0.4.5',
         description='Python notification system',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
