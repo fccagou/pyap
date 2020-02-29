b737a65230e06daed2b225d8c39d27f74bc45aaf
fccagou <fccagou@gmail.com>
2020-03-01 00:14:34 +0100

    rel(0.5.0)

--- setup-el6.py
+++ setup-el6.py
@@ -18,7 +18,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.4.9',
+        version='0.5.0',
         description='PYthon Alert Processor',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
--- setup-el7.py
+++ setup-el7.py
@@ -19,7 +19,7 @@
 
 from distutils.core  import setup
 setup(name='pyap',
-        version='0.4.9',
+        version='0.5.0',
         description='Python notification system',
         url='http://github.com/fccagou/pyap',
         author='fccagou',
